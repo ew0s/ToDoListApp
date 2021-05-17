2c3870fa0441f6bf64a4c34d9f6af183c3845467
ew0s <ewos365@mail.ru>
2021-05-17 18:40:06 +0300

    Add: initial project

---
+++ ToDo list/ToDo list/TaskListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo list
+//
+//  Created by Егор Савковский on 17.05.2021.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
